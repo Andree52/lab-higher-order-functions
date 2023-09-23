e9279002b7f81afcd58726cc889f3952486c45a1
Andree52 <andreearter@pursuit.org>
2023-09-23 11:59:31 -0400

    high order functions

--- index.js
+++ index.js
@@ -14,8 +14,11 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      return element; // Return the first element that satisfies the callback
+    }
   }
+  return undefined // Return undefined if no element satisfies the callback
 }
 
 /**
@@ -35,9 +38,11 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      result.push(element); // Push elements that satisfy the callback to the result array
+    }
   }
-  return result;
+  return result
 }
 
 /**
@@ -57,7 +62,8 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    const transformedElement = callback(element);
+    result.push(transformedElement)
   }
   return result;
 }
@@ -77,7 +83,7 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    callback(array[i], i, array); // Pass element, index, and array to the callback
   }
 }
 
